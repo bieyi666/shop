e63ec4aee89bc070b1ddad39bb81dfd5a40e4299
Administrator <3172449694@qq.com>
2020-12-16 18:55:20 +0800

    测试

--- shop-dao/src/main/java/com/shop/dao/CommodityTypeDao.java
+++ shop-dao/src/main/java/com/shop/dao/CommodityTypeDao.java
@@ -1,6 +1,16 @@
 package com.shop.dao;
 
+import com.shop.vo.CommodityType;
+
+import java.util.List;
+
 //商品类型
 public interface CommodityTypeDao {
+
+    /**
+     * 查询所有商品类型
+     * @return
+     */
+    public List<CommodityType> queryAllCommodityType();
 
 }
